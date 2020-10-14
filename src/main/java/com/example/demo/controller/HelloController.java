37cef1f50e3db50ba73e791a0d041971e1746798
Vu <vanvu2912@gmail.com>
2020-10-14 15:33:19 +0700

    feat(server): add sum feature
    feature to sum two number, n1 and n2

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -11,4 +11,10 @@ public class HelloController {
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
+
+    @GetMapping("/sum")
+    public String hello(@RequestParam(value = "n1", defaultValue = "0") int n1,
+                        @RequestParam(value = "n2", defaultValue = "0") int n2) {
+        return String.format("Result %s!", n1 + n2);
+    }
 }
