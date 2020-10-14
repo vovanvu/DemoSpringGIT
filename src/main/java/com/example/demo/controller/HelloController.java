af8257b28ad5abdd7daa4df50728e5cded18e052
Vu <vanvu2912@gmail.com>
2020-10-14 16:13:58 +0700

    aaa

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -16,6 +16,7 @@ public class HelloController {
     public String hello(@RequestParam(value = "n1", defaultValue = "0") int n1,
                         @RequestParam(value = "n2", defaultValue = "0") int n2) {
         System.out.println("hello from fea 2222");
+        System.out.println();
         return String.format("Result wrong %s!", n1 - n2);
     }
 }
